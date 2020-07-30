2d166dd9b715902c5726a55036051cb05b932d02
Yesika Gomez <yesika.go@hotmail.com>
2020-07-30 16:12:49 -0500

    Agregando el modal

---
+++ Js/main.js
@@ -0,0 +1,18 @@
+
+document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) =>{
+	if(document.getElementById("frm-mensualidad").reportValidity()){
+        let nombre, documento, genero, fecha_nac;
+    }else{
+        //alert("Error de validación de campos.")
+    }
+}
+);
+
+
+	
+    	
+$(document).ready((e) => {
+    $("#btn-adicionar").click((e) => {
+        $('#modal-adicionar').modal('toggle');
+    });
+});
