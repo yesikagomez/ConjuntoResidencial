b9d17ce3b8ec810c75c1bcdea6e21757625eea73
Yesika Gomez <yesika.go@hotmail.com>
2020-08-04 17:49:41 -0500

    creando clases

---
+++ Js/clases/personas.js
@@ -0,0 +1,5 @@
+class Persona{
+    constructor(){
+        console.log("Se creo una persona y este es el constructor");
+    }
+}
--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,33 @@
-const DATOS_FORMULARIO = []; 
+const DATOS_FORMULARIO = [
+    {
+        datos_propietario : {
+            nombre : "Juan Perez", 
+            documento : "89736363", 
+            genero : "H", 
+            fech_nac : "10-10-1980"
+        }, 
+        datos_inmueble : {
+            metos_cuadrados : 30, 
+            tipo : "C", 
+            numero_habitantes : 10, 
+            fech_mensualidad : "10-0-2020"
+        }
+    }
+];
 
-document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) =>{
+
+/*
+numeros.map(function(elemento, p){  
+	console.log("=====");
+    console.log(p, elemento);
+	console.log("=====");
+    return "posición: "+elemento;
+})
+[1,2,3,4,5]
+["posición: 1","posición: 2","posición: 3","posición: 4","posición: 5"]
+*/
+	
+document.getElementById("btn-guardar-inmueble").addEventListener('click', e => {
 	let nombre_almacenado, documento_almacenado, 
             genero_almacenado, fecha_nac_almacenada,mt_inmueble_almacenado,tipo_inmueble_almacenado, 
             num_habitantes_inmueble_almacenado, fech_men_inmueble_almacenado;
@@ -37,11 +64,12 @@ document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) =>
     }else{
         alert("Error de validación de campos.")
     }
-    
     DATOS_FORMULARIO.push(inmueble);
     document.getElementById("frm-mensualidad").reset();
     alert("Los datos fueron almacenados");
+    cargarDatosEnTabla();
     $('#modal-adicionar').modal('toggle');
+        
 });
    	
 $(document).ready((e) => {
@@ -49,3 +77,54 @@ $(document).ready((e) => {
         $('#modal-adicionar').modal('toggle');
     });
 });
+
+function cargarDatosEnTabla(){
+	document.querySelector("#tbl-mensualidades tbody").innerHTML = "";
+    DATOS_FORMULARIO.forEach( registro => {
+        document.querySelector("#tbl-mensualidades tbody").innerHTML += `<tr>
+                                                                        <td>${registro.datos_propietario.nombre}</td>
+                                                                        <td>${calcularEdad(new Date(registro.datos_propietario.fech_nac))} años</td>
+                                                                        <td>${registro.datos_propietario.genero === "F" ? "Mujer" : "Hombre"}</td>
+                                                                        <td>${0}</td>
+                                                                        <td>${registro.datos_inmueble.fech_mensualidad}</td>
+                                                                        <td><a>Ver</a></td>
+                                                                    <tr/>`;
+    });
+    
+}
+
+function calcularEdad(fecha_nacimiento){
+    let ageDifMs = Date.now() - fecha_nacimiento.getTime();
+    let ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+function recorrerVector(){
+    let arreglo = [3,5,6,8,1];
+    // for (let i = 0; i < arreglo.length; i++) {
+    //     console.log(arreglo[i]);      
+    // }
+    //let r =  arreglo.forEach( x => console.log(x) );
+    let r =  arreglo.map( x => x + 1);
+    console.log(arreglo);
+    console.log(r);
+}
+
+function condicionalReducido(){
+    let x, year = 2020;
+    //x = condicion ? valor_que_cumple : valor_alternativo;
+    x = year > 2000 ? 5 : 6;
+    // if(year > 2000){
+    //     x = 5;
+    // }else{
+    //     x = 6;
+    // }
+    return x;
+}
+
+cargarDatosEnTabla();
+//jsonModulo.funcionSaludar();
+//console.log("El nombre que se importo fue " + NOMBRE + " y la edad fue "+edad)
+	
+let X = new Estudiante(11, "11C",  "Diana", "Bedoya");
+console.log(X);
